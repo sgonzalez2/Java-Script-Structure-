a0912f84d04bc1e14f641a78f14620c7cc0d966a
sandy gonzalez <sandygonzalez012@gmail.com>
2019-02-28 01:30:58 -0500

    Learnig Variables

---
+++ Variables.js
@@ -0,0 +1,13 @@
+var firstName = 'Matt';
+let lastName = 'Landers';
+
+firstName = 'Will';
+lastName = 'Johnston'
+
+const fullName = 'Matt Landers';
+
+fullName = 'Will Johnston'
+
+console.log (firstName);
+console.log (lastName);
+console.log (fullName);
