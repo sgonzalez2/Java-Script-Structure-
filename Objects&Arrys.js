1e724cf8bb5e0e2d932cabca8276b08d090cbe41
sandy gonzalez <sandygonzalez012@gmail.com>
2019-02-28 05:02:35 -0500

    Objects

--- Objects&Arrys.js
+++ Objects&Arrys.js
@@ -72,3 +72,41 @@ lists = lists.concat ([3,4]);
 
 console.log(lists);
 //-->[2,4,6,7,10, 3, 4]
+
+//How Objects Work
+
+let post = {
+    title: 'Wellcome To My Blog',
+    catagory: 'Announcements',
+    body: 'Hello and welcome to my blog. I will posting about my adventures as a developer.',
+    tags: ['JS', 'CSS', 'HTML']
+};
+console.log(post.title);
+//--> 'Welcome To My Blog'
+
+console.log(post.tags[1]);
+//-->'CSS'
+
+let myObject = null;
+
+let my_Object = {
+    propA: 'value a',
+    propB: 'value b',
+     'pro c': 'value c'
+};
+
+let person = {
+    firstNames: 'John',
+    lastNames: 'Doe',
+};
+
+console.log(person.email);
+// --> Undefined
+
+person.email = 'Johp.Doe@gmail.com'
+console.log(person.email);
+//--> 'John.Doe.@gmail.com'
+
+
+console.log(Object.keys(person));
+--> ['firstNames', 'lastNames']
