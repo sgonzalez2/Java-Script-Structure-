8990d3705f3fee052faba647c92638cda8ca3b03
sandy gonzalez <sandygonzalez012@gmail.com>
2019-02-28 03:16:30 -0500

    how to use Arrys

---
+++ Objects&Arrys.js
@@ -0,0 +1,20 @@
+//How to use Arrays
+
+let list = [2,4,6,7,10];
+let index = 2;
+let a = 5;
+let b = -3;
+
+//Using Arrays lets you interact with data inside the collection
+
+//Select 6 to the console
+console.log(list[2]);
+
+//Select 6 to the console
+console.log(list[1+1]);
+
+//Select 6 to the console
+console.log(list[index]);
+
+//Select 6 to the console
+console.log(list[a+b]);
