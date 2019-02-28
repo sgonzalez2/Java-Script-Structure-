be5c5d383ba7aca9e5e4ab9e37ad63ca51ebd3c2
sandy gonzalez <sandygonzalez012@gmail.com>
2019-02-28 06:02:04 -0500

    Operations Used in JavaScrip

---
+++ Operators.js
@@ -0,0 +1,69 @@
+var val = 5+7;
+var a = 5*6;
+var d = 8/2;
+var m = 50 % 20;
+var t = 'Matt' + 'Landers';
+var s = 'Matt' + ' '+ 'Landers';
+var n = 'Matt' + 5;
+var k = '10' + 5;
+var q = '10' * 5;
+var w = '10a' * 5;
+var val1 = 25;
+var val2 = 30;
+var val3 = val1 + val2;
+var str1 = '25';
+var valu = 30;
+var equ = str1 + valu;
+var str2 = '5';
+var valu2 = 30;
+var equ2 = str2 * valu2;
+var e = 5;
+var r = 30;
+var y = e == r;
+var u = 6;
+var i = 6;
+var o = u==i;
+var p = 6;
+var f = 6;
+p += f;
+r -= e;
+o *= u;
+valu ++;
+console.log(val);
+//--> 12
+console.log(a);
+//--> 30
+console.log(d);
+//--> 4
+console.log(m);
+//--> 10
+console.log(t);
+//--> MattLanders
+console.log(s);
+//--> Matt Landers
+console.log(n);
+//--> Matt5
+console.log(k);
+//--> 105
+console.log(q);
+//--> 50
+console.log(w);
+//--> NaN
+console.log(val3);
+//--> 55
+console.log(equ);
+//--> 2530
+console.log(equ2);
+//--> 150
+console.log(y);
+//--> false
+console.log (o);
+//--> true
+console.log (p);
+//--> 4
+console.log(r);
+//--> 25
+console.log(o);
+//--> 6
+console.log(valu);
+//--> 31 
