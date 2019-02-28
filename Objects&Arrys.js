2b4b459c4669fa1f1627e03ddcdf1856cc1f0e5f
sandy gonzalez <sandygonzalez012@gmail.com>
2019-02-28 03:56:19 -0500

    Different Method Functions

--- Objects&Arrys.js
+++ Objects&Arrys.js
@@ -34,3 +34,41 @@ console.log(greeting['length']);
 //Select 11 to the console
 console.log(greeting[Property]);
 
+//Using Method Function
+
+let greetings = 'Hello World';
+
+//Log "function" to the console
+console.log(typeof greetings.toUpperCase);
+
+//Log "HELLO WORLD" to the console
+console.log(greetings.toUpperCase());
+
+//Methods for munipulating data using Arrays
+
+let lists =[2,4,6,7,10];
+
+console.log(lists);
+// --> [2,4,6,7,10]
+
+lists.push(3);
+
+console.log (lists);
+//--> [2,4,6,7,10, 3]
+
+console.log(lists.pop());
+//--> 3
+
+console.log(lists);
+//--> [2,4,6,7,10]
+
+console.log(lists.concat([3, 4]));
+//--> //--> [2,4,6,7,10,3,4]
+
+console.log(lists);
+//-->//--> [2,4,6,7,10]
+
+lists = lists.concat ([3,4]);
+
+console.log(lists);
+//-->[2,4,6,7,10, 3, 4]
