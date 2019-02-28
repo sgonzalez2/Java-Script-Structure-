6a3f702f12c2f97ffda26c556ae9ed56658f6b61
sandy gonzalez <sandygonzalez012@gmail.com>
2019-02-28 02:12:34 -0500

    Learning Data types

---
+++ DataTypes.js
@@ -0,0 +1,17 @@
+var firstName = "Matt";
+var age = 25;
+var isMale = true;
+var isfemale = false;
+var surname = null;
+
+var person = {
+'name': firstName,
+ age: age,
+ dob: '01/01/1970'
+};
+
+person.age;
+person.name;
+person.dob;
+
+console.log(person);
